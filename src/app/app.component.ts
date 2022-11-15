15f53154d9497a682643107b2f9c9f502534e78a
ghsoares <ghsoares99795@gmail.com>
2022-11-14 22:08:25 -0300

    Added navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { Chirp } from './models/chirp';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,23 @@ import { Chirp } from './models/chirp';
 })
 export class AppComponent {
   title = 'Chirper';
+  currentRoute: string = '';
+
+  constructor(
+    public userService: UserService,
+    private router: Router
+  ) { 
+    router.events.subscribe({
+      next: e => {
+        if (e instanceof NavigationEnd) {
+          this.currentRoute = e.urlAfterRedirects;
+        }
+      }
+    })
+  }
+  
+  navigateTo(...commands: any[]) {
+    this.router.navigate(commands);
+    this.currentRoute = this.router.url;
+  }
 }
