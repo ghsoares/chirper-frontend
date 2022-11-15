5ce6eb266385e0a79e7add74454858aef526f119
ghsoares <ghsoares99795@gmail.com>
2022-11-14 22:07:46 -0300

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateChirpComponent } from './components/tabs/create-chirp/create-chirp.component';
+import { HomeComponent } from './components/tabs/home/home.component';
+import { LoginComponent } from './components/tabs/login/login.component';
+import { SearchComponent } from './components/tabs/search/search.component';
+import { SignupComponent } from './components/tabs/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home' , pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'new-chirp', component: CreateChirpComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
