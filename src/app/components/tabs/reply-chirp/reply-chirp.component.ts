1609bcc98892d6c0991bfd56365ed7acb4609a46
ghsoares <ghsoares99795@gmail.com>
2022-11-14 22:12:33 -0300

    Added reply chirp component

---
+++ src/app/components/tabs/reply-chirp/reply-chirp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reply-chirp',
+  templateUrl: './reply-chirp.component.html',
+  styleUrls: ['./reply-chirp.component.scss']
+})
+export class ReplyChirpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
