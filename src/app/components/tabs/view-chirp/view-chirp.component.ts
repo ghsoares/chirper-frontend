6a8220be1d7b28c1c3ee51288ffc165cc75d46b1
ghsoares <ghsoares99795@gmail.com>
2022-11-14 22:14:08 -0300

    Added view chirp component

---
+++ src/app/components/tabs/view-chirp/view-chirp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-chirp',
+  templateUrl: './view-chirp.component.html',
+  styleUrls: ['./view-chirp.component.scss']
+})
+export class ViewChirpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
