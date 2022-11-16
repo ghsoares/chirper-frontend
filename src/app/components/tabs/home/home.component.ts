3d6399931828e038f70327b2c9f7d7a9939cfc84
ghsoares <ghsoares99795@gmail.com>
2022-11-15 22:30:24 -0300

    Some home changes

--- src/app/components/tabs/home/home.component.ts
+++ src/app/components/tabs/home/home.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Chirp } from 'src/app/models/chirp';
 import { ChirpService } from 'src/app/services/chirp.service';
+import { NavigationService } from 'src/app/services/navigation.service';
 import { UserService } from 'src/app/services/user.service';
+import { AlertService } from 'src/app/ui/alert/alert.service';
 
 @Component({
   selector: 'tab-home',
@@ -17,7 +19,8 @@ export class HomeComponent implements OnInit {
   constructor(
     public userService: UserService,
     private chirpService: ChirpService,
-    private router: Router
+    private navigation: NavigationService,
+    private alertService: AlertService
   ) { }
 
   ngOnInit(): void {
@@ -28,12 +31,20 @@ export class HomeComponent implements OnInit {
         this.chirps = chirps;
       },
       error: err => {
-        console.error(err);
+        this.alertService.error("Error when trying to get chirps", err.error?.message);
       }
     });
   }
 
   createChirp() {
-    this.router.navigate(['/new-chirp']);
+    this.navigation.navigate(['/new-chirp']);
+  }
+
+  viewChirp(chirpId: number): void {
+    this.navigation.navigate(['view-chirp'], {
+      queryParams: {
+        'chirp-id': chirpId
+      }
+    })
   }
 }
